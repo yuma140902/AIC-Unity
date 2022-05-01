e09536083dcb87b10b64a29011e9eb7dc3e63136
yuma140902 <yuma140902@gmail.com>
2022-05-01 20:46:22 +0900

    AIC3-part5

--- Assets/Script/PlayerContoller.cs
+++ Assets/Script/PlayerContoller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    public float Speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)) {
+            this.transform.position += this.transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            this.transform.position -= this.transform.forward * Speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += this.transform.right * Speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            this.transform.position -= this.transform.right * Speed * Time.deltaTime;
+        }
     }
 }
