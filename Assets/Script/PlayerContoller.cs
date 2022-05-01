d53236c1b53c31a5c2c8fef017e8b82795519485
yuma140902 <yuma140902@gmail.com>
2022-05-02 00:35:47 +0900

    AIC3-part9

--- Assets/Script/PlayerContoller.cs
+++ Assets/Script/PlayerContoller.cs
@@ -9,11 +9,13 @@ public class PlayerContoller : MonoBehaviour
     public GameObject Camera;
 
     float pitch;
+    float yaw;
 
     // Start is called before the first frame update
     void Start()
     {
         pitch = Camera.transform.localRotation.eulerAngles.x;
+        yaw = Camera.transform.localRotation.eulerAngles.y;
     }
 
     // Update is called once per frame
@@ -34,7 +36,14 @@ public class PlayerContoller : MonoBehaviour
 
         float mouseVertical = Input.GetAxis("Mouse Y");
         pitch += -mouseVertical * RotationSpeed;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        yaw += mouseHorizontal * RotationSpeed;
 
         Camera.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        this.transform.localRotation = Quaternion.Euler(0, yaw, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
