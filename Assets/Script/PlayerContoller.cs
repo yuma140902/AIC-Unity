496b9a7194c3cb022ab013bc6a0ba83b676014c8
yuma140902 <yuma140902@gmail.com>
2022-05-01 20:50:50 +0900

    AIC3-part7

--- Assets/Script/PlayerContoller.cs
+++ Assets/Script/PlayerContoller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     public float Speed;
+    public float RotationSpeed;
+    public GameObject Camera;
 
     // Start is called before the first frame update
     void Start()
